4d3c16bad3f9f0512265f715b4ad5779faa83680
kipanchal <kipanchal@stud.hs-heilbronn.de>
2025-01-19 18:29:09 +0100

    Initial Commit

---
+++ api/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+# Allow any origin (you can restrict this later for security reasons)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/api', methods=['GET'])
+def api():
+    return jsonify(message="Hello from the API!")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
